54e8072d2863da465b61af426ac62a1fcfe27447
daodaocheng <389231503@qq.com>
2021-05-21 17:22:36 +0800

    0521

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 //const path = require("path");
 const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
-//const resolve = dir => path.join(__dirname, dir);
+const resolve = dir => path.join(__dirname, dir);
 //const vantTheme = path.resolve(__dirname, "./src/assets/styles/theme.less");
 
 module.exports = {
-  publicPath: "/", // 公共路径
+  publicPath: "./", // 公共路径
   indexPath: "index.html", // 相对于打包路径index.html的路径
   outputDir: process.env.outputDir || "dist", // 'dist', 生产环境构建文件的目录
+  //outputDir: process.env.outputDir || "docs",
   assetsDir: "static", // 相对于outputDir的静态资源(js、css、img、fonts)目录
   lintOnSave: false, // 是否在开发环境下通过 eslint-loader 在每次保存时 lint 代码
   runtimeCompiler: true, // 是否使用包含运行时编译器的 Vue 构建版本
